d11b1197390614fcf3002ee0fa53c9e537f3b643
mattibal <mattibal@gmail.com>
2012-11-19 12:04:57 +0100

    aggiunto codice che gira sull'arduino

---
+++ JadeHomeAutomation/ArduinoSketches/SerProNetwork/SerProNetwork.ino
@@ -0,0 +1,79 @@
+#include "SerPro.h"
+#include "SerProPacket.h"
+#include "SerProHDLC.h"
+
+class SerialWrapper
+{
+public:
+	static inline void write(uint8_t v){
+		Serial.write(v);
+	};
+	static inline void flush(){
+	};
+};
+
+// 4 functions
+// 32 bytes maximum receive buffer size
+
+struct SerProConfig {
+	static unsigned int const maxFunctions = 4;
+	static unsigned int const maxPacketSize = 32;
+	static unsigned int const stationId = 3;
+};
+
+// SerialWrapper is class to handle tx
+// Name of class is SerPro
+// Protocol type is SerProPacket or SerProHDLC
+
+DECLARE_SERPRO(SerProConfig,SerialWrapper,SerProHDLC,SerPro);
+
+
+// --------- INIZIO FUNZIONI RPC CHIAMABILI DA REMOTO -----------
+
+// Funzione di test
+DECLARE_FUNCTION(0)(int a, int b, int c) {
+  Serial.println("aaaaaaaaaa");
+	SerPro::send(0, a+b+c);
+}
+END_FUNCTION
+
+DECLARE_FUNCTION(1)(int a, int b) {
+	SerPro::send(1, a+b);
+}
+END_FUNCTION
+
+DECLARE_FUNCTION(2)(void) {
+	SerPro::send(2);
+}
+END_FUNCTION
+
+DECLARE_FUNCTION(3)(int a, char *b) {
+	SerPro::send(3, b, a+1);
+	SerPro::send<uint8_t>(4, a+1);  // Enforce int argument to be a uint8_t
+}
+END_FUNCTION
+
+// --------- FINE FUNZIONI RPC CHIAMABILI DA REMOTO ------------
+
+
+// Implementation - 4 functions, name SerPro, protocol SerProHDLC.
+// Make sure this agrees with DECLARE statement.
+
+IMPLEMENT_SERPRO(4,SerPro,SerProHDLC);
+
+const int ledPin = 13;
+
+void setup()
+{
+	Serial.begin(9600);
+	pinMode(ledPin,OUTPUT);
+}
+
+
+
+void loop()
+{
+	if (Serial.available()>0) {
+		SerPro::processData(Serial.read());
+	}
+}
